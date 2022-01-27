4f64a263a7ff26c12725f8475c9b30ef53056d74
MajaVrh <majavrh5@gmail.com>
2022-01-28 00:29:20 +0100

    narudzbe ispravak commita

--- src/views/Narudzbe.vue
+++ src/views/Narudzbe.vue
@@ -1,16 +1,87 @@
 <template>
-  <div>
+  <div class="container">
     <sidebar />
+    <div class="sredina">
+      <h1 class="naslovStranice">NARUDŽBE</h1>
+      <div class="raspored">
+        <KarticaNarudzbe  v-for="KN in Narudzba" :key="KN.id" :NarudzbaStola="KN" />
+       
+    
+
+      
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
 import Sidebar from '@/components/Sidebar'
-
-export default { 
+import KarticaNarudzbe from '@/components/KarticaNarudzbe'
+import { db, collection, query, onSnapshot,   } from "@/firebase";
+export default {
   name: "Narudzbe",
-  components: {Sidebar}
-  };
+  data() {
+    return {
+      Narudzba:[],
+    };
+  },
+  mounted() {this.PrikazNarudzbe()},
+  
+  methods: {
+  async PrikazNarudzbe() {
+      const q = query(collection(db, "Narudzbe"));
+      try{
+      onSnapshot(q, (querySnapshot) => {
+        const CitajNarudzbu = [];
+        querySnapshot.forEach((doc) => {
+          CitajNarudzbu.push({ id: doc.id, ...doc.data() });
+        });
+        this.Narudzba = CitajNarudzbu;
+        console.log(this.Narudzba)
+      });}catch (error) {
+        console.log("Učitavanje narudžba nije uspijelo");
+      }
+    },
+
+  },
+  components: { KarticaNarudzbe,Sidebar },
+};
 </script>
 
-<style></style>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  display: flex;
+}
+
+.sredina {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 3rem auto;
+  max-width: 140vh;
+  width: 130vh;
+
+  padding-left: 25%;
+}
+
+.raspored {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2rem;
+
+  gap: 2rem;
+}
+.naslovStranice {
+  font-family: "Amatic SC", cursive;
+  font-size: 60px;
+  color: #731642;
+  font-weight: bold;
+}
+</style>
